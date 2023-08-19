23aaf8b458b7bedb2e84e36c806891d57e43cd19
~#M?x <89079595+suchsoak@users.noreply.github.com>
2023-08-18 23:28:26 -0400

    Update script.c

--- script.c
+++ script.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ncurses.h>
+#include <sys/utsname.h>
 
 int main()
 {
+
+    #ifdef __linux__
+            printf("You have Linux! COOL!\n");
+
     initscr(); 
     cbreak();  
-    noecho();  
-    keypad( stdscr, TRUE);    
-    start_color();
-
+    noecho();
+    refresh();  
+    keypad( stdscr, TRUE);
+    // start_color();
     int ch = getch();
-
+        
     while (TRUE)
     {
         COLOR_RED;
-        if ( ch == KEY_HOME){
-            system("firefox https://github.com/suchsoak");
+        if ( ch == KEY_F(2)){
+            system("\nfirefox");
+            system("clear");
             break;   
         }
         else{
-            printf("Another botton\n");
+            printf("\nanother button");
             break;
         }
         if (initscr == NULL){
@@ -29,6 +36,16 @@ int main()
         }
     }
     
-    endwin();
+
+    #elif _WIN32
+        printf("You use windows, man this not cool. Go to linux NOW!!\n");
+
+    #else
+        printf("Sorry, the system are"
+        "not listed above.\n");
+
+
+#endif
+endwin();
     return 0;
 }
